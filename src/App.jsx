fcb42e44afdd6311903e8d9d675c19c7848b9589
RubenGQ <rubengarquin@gmail.com>
2023-06-01 23:46:23 +0200

    se añade selector de ejemplos

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Canvas from "./Body/Canvas";
 import SideBar from "./Body/Sidebar";
 import Header from "./Header/Header";
-import {LASLoader} from '@loaders.gl/las';
-import {load} from '@loaders.gl/core';
+import { LASLoader } from '@loaders.gl/las';
+import { load } from '@loaders.gl/core';
 
 
 export default function App() {
-  const [data,setData]= useState()
-  const [colorType,setColorType] = useState('RGB')
-  const [loading,setLoading]  = useState(false)
+  const [data, setData] = useState()
+  const [colorType, setColorType] = useState('RGB')
+  const [loading, setLoading] = useState(false)
+  const [speed, setSpeed] = useState(1)
 
-  const readDataFromFile=async (file)=>{
+  const readDataFromFile = async (url,isUrl) => {
     setLoading(true)
-    let buffer= await file.arrayBuffer()
-    const datos = await load(buffer,LASLoader)
+    let file = isUrl ? await fetch(url) : url
+    let buffer = await file.arrayBuffer()
+    const datos = await load(buffer, LASLoader)
     setData(datos)
   }
 
-  const changeColorType=(selected)=>{
+  const changeColorType = (selected) => {
     setColorType(selected)
   }
 
@@ -28,9 +30,10 @@ export default function App() {
 
   return (
     <div className="w-full h-[100vh] bg-green-400 flex flex-col">
-      <Header readDataFromFile={readDataFromFile} className={"h-20 bg-blue-500"} />
+      <Header readDataFromFile={readDataFromFile} />
       <div className="grid h-full grid-cols-8">
-        <Canvas readDataFromFile={readDataFromFile} loading={loading} colorType={colorType}  data={data} />
+        <Canvas readDataFromFile={readDataFromFile} speed={speed} loading={loading} colorType={colorType} data={data}
+        />
         <SideBar changeColorType={changeColorType} />
       </div>
     </div>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,18 +1,71 @@
-// eslint-disable-next-line react/prop-types
+import React from 'react'
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
+
+const EXAMPLES_FILES = [
+  { name: 'half-dome', path: '/half-dome.laz' },
+  { name: 'st.Hellen', path: '/st.Hellen.laz' },
+  { name: 'iowa-bridge', path: '/iowa-bridge.laz' },
+  { name: 'spaceshuttle', path: '/spaceshuttle.laz' },
+]
+
+
 function Header(props) {
-  const handleNewFile=(event)=>{
-    props.readDataFromFile(event.target.files[0])
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleNewFile = (event) => {
+    props.readDataFromFile(event.target.files[0], false)
   }
 
-  return (
-    <div className={props.className}>
-          <input type="file" className="hidden" id="file-upload" onChange={handleNewFile}></input>
-      <label
-        className="cursor-pointer bg-white border border-gray-300 text-black-700 py-2 px-4 rounded-lg shadow-sm  hover:bg-gray-100" htmlFor="file-upload">
-        Seleccionar archivo
-      </label>
+  const handleExampleFile = (url) => {
+    setDropdown(!dropdown)
+    props.readDataFromFile(url, true)
 
+  }
+
+
+
+
+  return (
+    <div className='relative'>
+      <div className={" flex items-center py-3 h-30 bg-blue-500"} >
+        <input type="file" className=" hidden" id="file-upload" onChange={handleNewFile}></input>
+        <label
+          className="cursor-pointer bg-white border border-gray-300 text-black-700 ml-3 py-2 px-4 rounded-lg shadow-sm  hover:bg-gray-100">
+          Seleccionar archivo
+        </label>
+        <button
+          className=" ml-0 flex items-center cursor-pointer bg-white border border-gray-300 text-black-700 h-full px-1 py-3 rounded-lg shadow-sm hover:bg-gray-100"
+          onClick={() => setDropdown(!dropdown)}
+        >
+          <FontAwesomeIcon icon={faAngleDown} className="mx-auto " />
+        </button>
+
+        {dropdown && (
+          <div className="absolute top-full left-0 mt-2 bg-white border border-gray-300 py-2 px-4 rounded-lg shadow-sm">
+            <p className="text-gray-700">Archivos de ejemplo:</p>
+            <ul className="mt-2">
+
+              {EXAMPLES_FILES.map((file, index) => (
+
+                <li key={index}
+                  className="cursor-pointer text-black font-mono hover:text-blue-700 border-b border-gray-300"
+                  onClick={() => handleExampleFile(file.path)}
+                >
+                  {file.name}
+                </li>
+              ))}
+
+            </ul>
+          </div>
+        )}
+
+      </div>
     </div>
+
   );
 }
 
